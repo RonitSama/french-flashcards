7b69c4cfc7d28a0bd4074b8ed8cc1837eda7e060
RonitSama <98502677+RonitSama@users.noreply.github.com>
2023-11-08 20:37:16 -0500

    Update flashcards.py

--- src/flashcards.py
+++ src/flashcards.py
@@ -1,10 +1,19 @@
-from tkinter import *
+__author__ = "Ronit Samanta"
+
+from tkinter import Tk, Canvas, PhotoImage, Button
 import random
 from words import words
 
 BACKGROUND_COLOR = "#B1DDC6"
-current_word: dict[str, str]
+
 timer = 'after#0'
+current_word: dict[str, str]
+canvas: Canvas
+window: Tk
+front_img: PhotoImage
+back_img: PhotoImage
+wrong_img: PhotoImage
+right_img: PhotoImage
 
 
 def new_word():
@@ -22,11 +31,11 @@ def new_word():
         canvas.itemconfig(1, image=front_img)
         return
 
+    canvas.itemconfig(1, image=front_img)
     canvas.itemconfig(2, text='Infinitive', fill='black')
     canvas.itemconfig(3, text=current_word['Infinitive'], fill='black')
-    canvas.itemconfig(1, image=front_img)
 
-    timer = window.after(3000, flip_card)
+    timer = window.after(6000, flip_card)
 
 
 def flip_card():
@@ -35,37 +44,43 @@ def flip_card():
     canvas.itemconfig(3, text=current_word['Conjugated'], fill='white')
 
 
-# window
-window = Tk()
-window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
-window.title('French Flashcards')
+def main():
+    global canvas, window, front_img, back_img, wrong_img, right_img
 
-# images
-front_img = PhotoImage(file='./images/card_front.png')
-back_img = PhotoImage(file='./images/card_back.png')
-wrong_img = PhotoImage(file='./images/wrong.png')
-right_img = PhotoImage(file='./images/right.png')
+    # window
+    window = Tk()
+    window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+    window.title('French Flashcards')
 
-# canvas
-canvas = Canvas(height=526, width=800,
-                bg=BACKGROUND_COLOR, highlightthickness=0)
+    # images
+    front_img = PhotoImage(file='../images/card_front.png')
+    back_img = PhotoImage(file='../images/card_back.png')
+    wrong_img = PhotoImage(file='../images/wrong.png')
+    right_img = PhotoImage(file='../images/right.png')
 
-# flashcards
-canvas.create_image(400, 263, image=front_img)
-canvas.create_text(400, 150, text='French',
-                   font=('Arial', 40, 'italic'), fill='black')
-canvas.create_text(400, 300,
-                   font=('Arial', 60, 'bold'), fill='black', width=600)
-canvas.grid(row=0, column=0, columnspan=2)
+    # canvas
+    canvas = Canvas(height=526, width=800,
+                    bg=BACKGROUND_COLOR, highlightthickness=0)
 
-# buttons
-wrong_button = Button(image=wrong_img, highlightthickness=0,
-                      borderwidth=0, command=new_word)
-wrong_button.grid(row=1, column=0)
-right_button = Button(image=right_img, highlightthickness=0,
-                      borderwidth=0, command=new_word)
-right_button.grid(row=1, column=1)
+    # flashcards
+    canvas.create_image(400, 263, image=front_img)
+    canvas.create_text(400, 150, text='French',
+                    font=('Arial', 40, 'italic'), fill='black')
+    canvas.create_text(400, 300,
+                    font=('Arial', 60, 'bold'), fill='black', width=600)
+    canvas.grid(row=0, column=0, columnspan=2)
 
-new_word()
+    # buttons
+    wrong_button = Button(image=wrong_img, highlightthickness=0,
+                        borderwidth=0, command=new_word)
+    wrong_button.grid(row=1, column=0)
+    right_button = Button(image=right_img, highlightthickness=0,
+                        borderwidth=0, command=new_word)
+    right_button.grid(row=1, column=1)
 
-window.mainloop()
+    new_word()
+
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
